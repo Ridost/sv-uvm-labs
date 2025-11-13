5300e5a8cff9e0029b41251d531e5c25c6b86500
Min-Hsiang Liao <35066190+Ridost@users.noreply.github.com>
2025-11-14 01:01:41 +0800

    Create my_sequence.sv

---
+++ lab01/my_sequence.sv
@@ -0,0 +1,22 @@
+class my_sequence extends uvm_sequence #(my_transaction);
+  `uvm_object_utils(my_sequence)
+  
+  function new (string name = "my_sequence");
+    super.new(name);
+  endfunction
+  
+  virtual task body();
+    if (starting_phase != null)begin
+      starting_phase.raise_objection(this);
+    end
+    
+    repeat(10)begin
+      `uvm_do(req)
+    end
+    
+    #100;
+    if(starting_phase != null)begin
+      starting_phase.drop_objection(this);
+    end
+  endtask
+endclass
