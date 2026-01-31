0767c1b83ffb61ac41d5827eb4c0d2043103e6bf
Min-Hsiang Liao <35066190+Ridost@users.noreply.github.com>
2026-01-31 15:47:51 +0800

    Create sequence_item.sv

---
+++ sync_fifo/tb/sequence_item.sv
@@ -0,0 +1,18 @@
+`include "uvm_macros.svh"
+
+class fifo_seq_item extends uvm_sequence_item;
+  `uvm_object_utils(fifo_seq_item)
+  
+  rand bit [7:0]data_in;
+  rand bit rd_en;
+  rand bit wr_en;
+  bit full;
+  bit empty;
+  bit [7:0] data_out;
+  
+  function new(string name="fifo_seq_item");
+    super.new(name);
+  endfunction
+  
+  
+endclass
